b309997f1ffa38968be01a85e9330a06052e3d7a
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2022-06-09 00:59:04 -0500

    create window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
